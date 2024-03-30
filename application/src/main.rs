fa8027d1eb59d8b7f642312bea9a3a95aaae219d
Jack Hogan <jackhogan11@gmail.com>
2024-03-29 22:39:39 -0400

    Initial multicore

--- application/src/main.rs
+++ application/src/main.rs
@@ -1,19 +1,73 @@
 #![no_std]
 #![no_main]
 
-use embassy_executor::Spawner;
+use defmt::*;
+use embassy_executor::Executor;
 use embassy_time::{Duration, Timer};
 use {defmt_rtt as _, panic_probe as _};
+use embassy_rp::{gpio::{Level, Output, Pin}, multicore::{spawn_core1, Stack}};
+use embassy_sync::{blocking_mutex::raw::CriticalSectionRawMutex, channel::Channel};
+use static_cell::StaticCell;
 
 const WIFI_SSID: &str = env!("WIFI_SSID");
 const WIFI_USERNAME: &str = env!("WIFI_USERNAME");
 const WIFI_PASSWORD: &str = env!("WIFI_PASSWORD");
 
-#[embassy_executor::main]
-async fn main(_spawner: Spawner) {
-    let _p = embassy_rp::init(Default::default());
+// #[embassy_executor::main]
+// async fn main(_spawner: Spawner) {
+//     let _p = embassy_rp::init(Default::default());
+//     loop {
+//         defmt::info!("Blink");
+//         Timer::after(Duration::from_millis(100)).await;
+//     }
+// }
+
+static mut CORE1_STACK: Stack<4096> = Stack::new();
+static EXECUTOR0: StaticCell<Executor> = StaticCell::new();
+static EXECUTOR1: StaticCell<Executor> = StaticCell::new();
+static CHANNEL: Channel<CriticalSectionRawMutex, LedState, 1> = Channel::new();
+
+enum LedState {
+    On,
+    Off,
+}
+
+#[cortex_m_rt::entry]
+fn main() -> ! {
+    let p = embassy_rp::init(Default::default());
+    let led = Output::new(p.PIN_25, Level::Low);
+
+    spawn_core1(
+        p.CORE1,
+        unsafe { &mut *core::ptr::addr_of_mut!(CORE1_STACK) },
+        move || {
+            let executor1 = EXECUTOR1.init(Executor::new());
+            executor1.run(|spawner| unwrap!(spawner.spawn(core1_task(led))));
+        },
+    );
+
+    let executor0 = EXECUTOR0.init(Executor::new());
+    executor0.run(|spawner| unwrap!(spawner.spawn(core0_task())));
+}
+
+#[embassy_executor::task]
+async fn core0_task() {
+    info!("Hello from core 0");
     loop {
-        defmt::info!("Blink");
-        Timer::after(Duration::from_millis(100)).await;
+        CHANNEL.send(LedState::On).await;
+        Timer::after_millis(100).await;
+        CHANNEL.send(LedState::Off).await;
+        Timer::after_millis(400).await;
+    }
+}
+
+#[embassy_executor::task]
+async fn core1_task(mut led: Output<'static, impl Pin>) {
+    info!("Hello from core 1");
+    loop {
+        match CHANNEL.receive().await {
+            LedState::On => led.set_high(),
+            LedState::Off => led.set_low(),
+        }
     }
 }
